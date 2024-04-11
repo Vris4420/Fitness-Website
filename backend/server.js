2440c1122b0942a95d7c8e8571ade2786c329a71
Vrishank Sharma <sharma08462@gmail.com>
2024-04-12 00:25:20 +0530

    Express Router Setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
+const workoutRoutes = require("./routes/workout");
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome" });
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("server is listening");
